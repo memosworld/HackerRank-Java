0ecee3cd0e664572ee545f8e9b7fcd9a2dec5efc
Memo's World <itsmemosworld@gmail.com>
2021-07-24 14:57:04 +0300

    Solutions for challanges 11-13
    Introduction part is finished.

---
+++ src/memos/world/_13_Java_Currency_Formatter/Solution.java
@@ -0,0 +1,26 @@
+package memos.world._13_Java_Currency_Formatter;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// https://www.hackerrank.com/challenges/java-currency-formatter/problem
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
